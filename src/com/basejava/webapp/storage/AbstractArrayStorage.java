097e2f65c1d5034ee8aa0f5e323c7df178860abd
amarkovkin <markovkin88@gmail.com>
2022-06-11 23:11:00 +0300

    10. Выносит шаблонные методы в абстрактный класс, чтобы сократить дублирование кода

---
+++ src/com/basejava/webapp/storage/AbstractArrayStorage.java
@@ -0,0 +1,81 @@
+package com.basejava.webapp.storage;
+
+import com.basejava.webapp.model.Resume;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public abstract class AbstractArrayStorage implements Storage {
+    protected static final int STORAGE_LIMIT = 10000;
+    protected final Resume[] storage = new Resume[STORAGE_LIMIT];
+
+    private int size = 0;
+
+    public int size() {
+        return size;
+    }
+
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+        System.out.println("Хранилище очищено");
+    }
+
+    public Resume get(String uuid) {
+        int index = findIndex(uuid);
+        if (index >= 0) {
+            return storage[index];
+        }
+        return null;
+    }
+
+    public void update(Resume r) {
+        int index = findIndex(r.getUuid());
+        if (index >= 0) {
+            storage[index].setUuid(storage[index].getUuid() + "U");
+            System.out.format("В резюме с индексом %d обновлен uuid: %s\n", index, storage[index].getUuid());
+        }
+    }
+
+    public void save(Resume r) {
+        Objects.requireNonNull(r);
+        int index = findIndex(r.getUuid());
+        System.out.format("current index %d of resume with uuid%s\n", index, r.getUuid());
+        if (size == STORAGE_LIMIT) {
+            String msgForOverflow = "Внимание! В хранилище - нет свободного места. \n" +
+                    "Резюме с uuid %s добавить не удалось. " +
+                    "Попробуйте удалить неиспользуемые резюме\n\n";
+            System.out.format(msgForOverflow, r.getUuid());
+        } else if (index >= 0) {
+            System.out.format("Резюме с %s уже есть в базе\n", r.getUuid());
+        } else {
+            storage[size] = r;
+            size++;
+        }
+    }
+
+    public void delete(String uuid) {
+        int index = findIndex(uuid);
+        if (index >= 0) {
+            System.out.format("Резюме с uuid: %s удалено%n", uuid);
+            sortAfterDelete(index);
+            storage[size - 1] = null;
+            size--;
+        }
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, size);
+    }
+
+    public int getLength() {
+        return STORAGE_LIMIT;
+    }
+
+    protected abstract void sortAfterDelete(int index);
+
+    protected abstract int findIndex(String uuid);
+}
