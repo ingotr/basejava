0eb1a4e4df53de2073f56b19908587817d9d406b
Markovkin Aleksandr <markovkin88@gmail.com>
2022-06-17 09:33:26 +0300

    19. fix: упрощает вызов findIndex с учетом нового конструктор Resume(uuid)

--- src/com/basejava/webapp/storage/SortedArrayStorage.java
+++ src/com/basejava/webapp/storage/SortedArrayStorage.java
@@ -13,9 +13,7 @@ public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     protected int findIndex(String uuid) {
-        Resume searchKey = new Resume();
-        searchKey.setUuid(uuid);
-        int index = Arrays.binarySearch(storage, 0, size(), searchKey);
+        int index = Arrays.binarySearch(storage, 0, size(), new Resume(uuid));
         if(index < 0) {
             System.out.format("Резюме с uuid: %s нет в хранилище\n", uuid);
         }
