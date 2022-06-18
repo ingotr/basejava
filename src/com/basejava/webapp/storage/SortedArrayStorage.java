b9d3e8e0f2527acd5f487e937f19b7570f0ad01e
amarkovkin <markovkin88@gmail.com>
2022-06-18 12:36:00 +0300

    23. fix: упрощает реализацию findIndex

--- src/com/basejava/webapp/storage/SortedArrayStorage.java
+++ src/com/basejava/webapp/storage/SortedArrayStorage.java
@@ -13,11 +13,7 @@ public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     protected int findIndex(String uuid) {
-        int index = Arrays.binarySearch(storage, 0, size(), new Resume(uuid));
-        if(index < 0) {
-            System.out.format("Резюме с uuid: %s нет в хранилище\n", uuid);
-        }
-        return index;
+        return Arrays.binarySearch(storage, 0, size(), new Resume(uuid));
     }
 
     @Override
