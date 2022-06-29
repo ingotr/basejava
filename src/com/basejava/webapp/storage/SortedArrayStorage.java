eea290485965a879a49cdc44be52d5e01452546d
Aleksandr Markovkin <markovkin88@gmail.com>
2022-06-29 15:03:20 +0300

    48. fix: добавляет компаратор в метод getSearchKey

--- src/com/basejava/webapp/storage/SortedArrayStorage.java
+++ src/com/basejava/webapp/storage/SortedArrayStorage.java
@@ -7,12 +7,7 @@ import java.util.Comparator;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
 
-    private static final Comparator<Resume> RESUME_COMPARATOR = new Comparator<Resume>() {
-        @Override
-        public int compare(Resume o1, Resume o2) {
-            return o1.getUuid().compareTo(o2.getUuid());
-        }
-    };
+    private static final Comparator<Resume> RESUME_COMPARATOR = Comparator.comparing(Resume::getUuid);
 
     @Override
     protected void fillDeletedElement(int index) {
@@ -21,7 +16,8 @@ public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     protected Integer getSearchKey(String uuid) {
-        return Arrays.binarySearch(storage, 0, size(), new Resume(uuid));
+        Resume searchKey = new Resume(uuid, "dummy");
+        return Arrays.binarySearch(storage, 0, size, searchKey, RESUME_COMPARATOR);
     }
 
     @Override
