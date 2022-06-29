91f795afd1b3d73b24c3cb4c0e187986a04ed461
Aleksandr Markovkin <markovkin88@gmail.com>
2022-06-29 11:49:13 +0300

    46. добавляет компаратор

--- src/com/basejava/webapp/storage/SortedArrayStorage.java
+++ src/com/basejava/webapp/storage/SortedArrayStorage.java
@@ -3,8 +3,16 @@ package com.basejava.webapp.storage;
 import com.basejava.webapp.model.Resume;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
+
+    private static final Comparator<Resume> RESUME_COMPARATOR = new Comparator<Resume>() {
+        @Override
+        public int compare(Resume o1, Resume o2) {
+            return o1.getUuid().compareTo(o2.getUuid());
+        }
+    };
 
     @Override
     protected void fillDeletedElement(int index) {
