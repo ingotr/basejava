528c332614f237d870634c570c8b301c45347f19
Aleksandr Markovkin <markovkin88@gmail.com>
2022-07-07 15:47:51 +0300

    54. fix: обновляет перечисления

--- src/com/basejava/webapp/model/ContactType.java
+++ src/com/basejava/webapp/model/ContactType.java
@@ -3,11 +3,13 @@ package com.basejava.webapp.model;
 public enum ContactType {
     EMAIL("электронная почта"),
     MOBILE_PHONE("мобильный телефон"),
-    GITHUB("github профиль"),
-    STACKOVERFLOW("stackoverflow профиль"),
+    GITHUB("профиль github"),
+    LINKEDIN("профиль linkedin"),
+    STACKOVERFLOW("профиль stackoverflow"),
     HOMEPAGE("домашняя страница"),
     TELEGRAM("телеграм"),
     SLACK("slack"),
+    SKYPE("skype"),
     ;
 
     ContactType(String value) {
--- src/com/basejava/webapp/model/Resume.java
+++ src/com/basejava/webapp/model/Resume.java
@@ -1,5 +1,7 @@
 package com.basejava.webapp.model;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -11,6 +13,9 @@ public class Resume implements Comparable<Resume> {
     // Unique identifier
     private final String uuid;
     private final String fullName;
+
+    private final Map<SectionType, Section> sections = new HashMap<>();
+    private final Map<ContactType, String> contacts = new HashMap<>();
 
     public Resume(String fullName) {
         this(UUID.randomUUID().toString(), fullName);
@@ -27,6 +32,42 @@ public class Resume implements Comparable<Resume> {
 
     public String getFullName() {
         return fullName;
+    }
+
+    public void addContact(ContactType type, String contact) {
+        contacts.put(type, contact);
+    }
+
+    public Map<ContactType, String> getContacts() {
+        return contacts;
+    }
+
+    public void addSection(SectionType type) {
+        switch (type) {
+            case PERSONAL:
+            case OBJECTIVE:
+                sections.put(type, new TextSection(type));
+                break;
+            case ACHIEVMENTS:
+            case QUALIFICATIONS:
+                sections.put(type, new ListSection(type));
+                break;
+            case EXPERIENCE:
+            case EDUCATION:
+                sections.put(type, new ObjectSection(type));
+                break;
+            default:
+                break;
+        }
+    }
+
+    public Section getSection(SectionType type) {
+        return sections.get(type);
+    }
+
+
+    public Map<SectionType, Section> getSections() {
+        return sections;
     }
 
     @Override
