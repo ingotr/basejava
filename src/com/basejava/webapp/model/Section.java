37ef2c1eb55cfa816379dbb3863df4865c952e84
Aleksandr Markovkin <markovkin88@gmail.com>
2022-07-10 13:05:11 +0300

    65. refactor: убирает лишние поля и конструкторы

--- src/com/basejava/webapp/model/Section.java
+++ src/com/basejava/webapp/model/Section.java
@@ -1,13 +1,4 @@
 package com.basejava.webapp.model;
 
 public abstract class Section {
-    private final SectionType type;
-
-    public Section(SectionType type) {
-        this.type = type;
-    }
-
-    public SectionType getType() {
-        return type;
-    }
 }
