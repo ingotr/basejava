c5d9ef7671b35fe70c969ae81885ba9406be080c
Aleksandr Markovkin <markovkin88@gmail.com>
2022-07-12 22:14:09 +0300

    80. добавляет реализацию методов doWrite, doRead

--- src/com/basejava/webapp/storage/ObjectStreamPathStorage.java
+++ src/com/basejava/webapp/storage/ObjectStreamPathStorage.java
@@ -1,19 +1,31 @@
 package com.basejava.webapp.storage;
 
+import com.basejava.webapp.exception.StorageException;
 import com.basejava.webapp.model.Resume;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.nio.file.Path;
 
-public class ObjectStreamPathStorage extends AbstractFileStorage{
+public class ObjectStreamPathStorage extends AbstractPathStorage{
+    protected ObjectStreamPathStorage(Path directory) {
+        super(String.valueOf(directory));
+    }
+
     @Override
-    protected void doWrite(Resume r, OutputStream os) throws IOException {
-
+    protected void doWrite(Resume r, OutputStream os) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(os)) {
+            oos.writeObject(r);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     protected Resume doRead(InputStream is) throws IOException {
-        return null;
+        try (ObjectInputStream ois = new ObjectInputStream(is)) {
+            return (Resume) ois.readObject();
+        } catch (ClassNotFoundException e) {
+            throw new StorageException("Error read resume", null, e);
+        }
     }
 }
