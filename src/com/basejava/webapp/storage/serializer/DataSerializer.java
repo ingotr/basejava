d109a4a95a57fc941e375f87b148301206ddbc31
Aleksandr Markovkin <markovkin88@gmail.com>
2022-07-18 10:41:53 +0300

    107. добавляет обход секций

--- src/com/basejava/webapp/storage/serializer/DataSerializer.java
+++ src/com/basejava/webapp/storage/serializer/DataSerializer.java
@@ -1,11 +1,10 @@
 package com.basejava.webapp.storage.serializer;
 
-import com.basejava.webapp.model.ContactType;
-import com.basejava.webapp.model.Resume;
-import com.basejava.webapp.model.Section;
-import com.basejava.webapp.model.SectionType;
-
+import com.basejava.webapp.model.*;
 import java.io.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class DataSerializer implements Serializer {
@@ -24,10 +23,42 @@ public class DataSerializer implements Serializer {
             // TODO implements sections
             Map<SectionType, Section> sections = r.getSections();
             dos.writeInt(sections.size());
-//            for (Map.Entry<SectionType, Section> entry : sections.entrySet()) {
-//                dos.writeUTF(entry.getKey().name());
-//                dos.writeUTF(String.valueOf(entry.getValue()));
-//            }
+            for (Map.Entry<SectionType, Section> entry : sections.entrySet()) {
+                String type = entry.getKey().name();
+                switch (type) {
+                    case "OBJECTIVE":
+                    case "PERSONAL":
+                        dos.writeUTF(entry.getKey().name());
+                        dos.writeUTF(String.valueOf(entry.getValue()));
+                        break;
+                    case "ACHIEVEMENTS":
+                    case "QUALIFICATIONS":
+                        dos.writeUTF(entry.getKey().name());
+                        List<String> content = ((ListSection)(entry.getValue())).getList();
+                        dos.writeInt(content.size());
+                        for (String line : content) {
+                            dos.writeUTF(line);
+                        }
+                        break;
+                    case "EXPERIENCE":
+                    case "EDUCATION":
+                        dos.writeUTF(entry.getKey().name());
+                        List<Organization> organizations = ((OrganizationSection)(entry.getValue())).getOrganizations();
+                        dos.writeInt(organizations.size());
+                        for (Organization org : organizations) {
+                            dos.writeUTF(org.getTitle());
+                            dos.writeUTF(org.getWebsite());
+                            dos.writeInt(org.getPeriods().size());
+                            for (Period period : org.getPeriods()) {
+                                dos.writeUTF(String.valueOf(period.getStartDate()));
+                                dos.writeUTF(String.valueOf(period.getEndDate()));
+                                dos.writeUTF(period.getPosition());
+                                dos.writeUTF(period.getDuties());
+                            }
+                        }
+                        break;
+                }
+            }
         }
     }
 
@@ -43,10 +74,58 @@ public class DataSerializer implements Serializer {
             }
             // TODO implements sections
             int sectionsSize = dis.readInt();
-            System.out.println(sectionsSize);
-//            for (int i = 0; i < sectionsSize; i++) {
-//                resume.addSection(SectionType.valueOf(dis.readUTF()), dis.readUTF());
-//            }
+            //System.out.println(sectionsSize);
+            for (int i = 0; i < sectionsSize; i++) {
+                String type = dis.readUTF();
+                switch (type) {
+                    case "OBJECTIVE":
+                    case "PERSONAL":
+                        String text = dis.readUTF();
+                        resume.addSection(SectionType.valueOf(type), text);
+                        break;
+                    case "ACHIEVEMENTS":
+                    case "QUALIFICATIONS":
+                        int contentSize = dis.readInt();
+                        List<String> content = new ArrayList<>();
+                        for(int j = 0; j < contentSize; j++) {
+                            content.add(dis.readUTF());
+                        }
+                        ListSection listSection = new ListSection();
+                        listSection.addAllToList(content);
+                        resume.addSection(SectionType.valueOf(type), listSection);
+                        break;
+                    case "EXPERIENCE":
+                    case "EDUCATION":
+                        OrganizationSection organizationSection = new OrganizationSection();
+                        int organizationsSize = dis.readInt();
+                        List<Organization> organizations = new ArrayList<>();
+                        for (int j = 0; j < organizationsSize; j++) {
+                            String title = dis.readUTF();
+                            String website = dis.readUTF();
+//                            System.out.println("title: " + title);
+//                            System.out.println("website: " + website);
+                            Organization newOrg = new Organization(title, website);
+                            int periodsSize = dis.readInt();
+                            for (int k = 0; k < periodsSize; k++) {
+                                LocalDate startdate = LocalDate.parse(dis.readUTF());
+                                LocalDate enddate = LocalDate.parse(dis.readUTF());
+//                                System.out.println("stardate: " + startdate);
+//                                System.out.println("enddate: " + enddate);
+                                String position = dis.readUTF();
+                                String duties = dis.readUTF();
+//                                System.out.println("position: " + position);
+//                                System.out.println("duties: " + duties);
+                                newOrg.addPeriod(startdate, enddate, position, duties);
+                            }
+                            //System.out.println(newOrg);
+                            organizations.add(newOrg);
+                        }
+                        organizationSection.addAllOrganizations(organizations);
+                        //System.out.println(organizationSection);
+                        resume.addSection(SectionType.valueOf(type), organizationSection);
+                        break;
+                }
+            }
             return resume;
         }
     }
