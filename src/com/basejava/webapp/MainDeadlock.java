d9359ac04373f8b76a91c8d4502abb262f672b0b
Aleksandr Markovkin <markovkin88@gmail.com>
2022-07-20 19:27:28 +0300

    117. refactor: Добавляет sleep в метод run. Переписывает потоки в виде одного класса

--- src/com/basejava/webapp/MainDeadlock.java
+++ src/com/basejava/webapp/MainDeadlock.java
@@ -4,38 +4,53 @@ public class MainDeadlock {
     public static final Object LOCK1 = new Object();
     public static final Object LOCK2 = new Object();
 
+    public static final String THREAD1_NAME = "Thread1";
+    public static final String THREAD2_NAME = "Thread2";
+    public static final String LOCK1_NAME = "LOCK1";
+    public static final String LOCK2_NAME = "LOCK2";
+
+
     public static void main(String[] args) {
-        Thread1 thread1 = new Thread1();
-        Thread2 thread2 = new Thread2();
+        Thread1 thread1 = new Thread1(LOCK1, LOCK2, THREAD1_NAME, LOCK1_NAME, LOCK2_NAME);
+        Thread1 thread2 = new Thread1(LOCK2, LOCK1, THREAD2_NAME, LOCK2_NAME, LOCK1_NAME);
         thread1.start();
         thread2.start();
     }
 }
 
 class Thread1 extends Thread {
-    public void run() {
-        System.out.println("Thread1: попытка захватить монитора объектак LOCK1");
-        synchronized (MainDeadlock.LOCK1) {
-            System.out.println("Thread1: монитор объекта LOCK1 захвачен");
 
-            System.out.println("Thread1: попытка захватить монитора объектак LOCK2");
-            synchronized (MainDeadlock.LOCK2) {
-                System.out.println("Thread1: мониторы объектов LOCK1 и LOCK2 захвачены");
+    private final Object lock1;
+    private final Object lock2;
+    private final String threadName;
+    private final String lock1Name;
+    private final String lock2Name;
+
+    public Thread1(Object lock1, Object lock2, String threadName, String lock1Name, String lock2Name) {
+        this.lock1 = lock1;
+        this.lock2 = lock2;
+        this.threadName = threadName;
+        this.lock1Name = lock1Name;
+        this.lock2Name = lock2Name;
+    }
+
+    public void run() {
+        System.out.println(threadName + ": попытка захватить монитор объекта " + lock1Name);
+        synchronized (lock1) {
+            System.out.println(threadName + ": монитор объекта " + lock1Name + " захвачен");
+            System.out.println(threadName + ": попытка захватить монитор объекта " + lock2Name);
+            threadSleep();
+            synchronized (lock2) {
+                System.out.println(threadName + ": мониторы объектов " + lock1Name + " и " + lock2Name + " захвачены");
             }
         }
     }
-}
 
-class Thread2 extends Thread {
-    public void run() {
-        System.out.println("Thread2: попытка захватить монитора объектак LOCK2");
-        synchronized (MainDeadlock.LOCK2) {
-            System.out.println("Thread2: монитор объекта LOCK2 захвачен");
-
-            System.out.println("Thread2: попытка захватить монитора объектак LOCK1");
-            synchronized (MainDeadlock.LOCK1) {
-                System.out.println("Thread2: мониторы объектов LOCK1 и LOCK2 захвачены");
-            }
+    private void threadSleep() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 }
