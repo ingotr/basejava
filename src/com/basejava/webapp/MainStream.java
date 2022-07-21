35b57cd8a905dafe2fc36ba363a493464ee7cb8d
Aleksandr Markovkin <markovkin88@gmail.com>
2022-07-21 13:00:10 +0300

    118. добавляет методы minValue, oddOrEven

---
+++ src/com/basejava/webapp/MainStream.java
@@ -0,0 +1,39 @@
+package com.basejava.webapp;
+
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class MainStream {
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 1, 1, 2, 5, 4};
+        System.out.println(minValue(nums));
+
+        List<Integer> integers = Arrays.stream(nums).boxed().collect(Collectors.toList());
+        System.out.println(oddOrEven(integers));
+    }
+
+    public static String minValue(int[] values) {
+        return Arrays.stream(values)
+                .distinct()
+                .sorted()
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining());
+    }
+
+    public static List<Integer> oddOrEven(List<Integer> integers) {
+        int sum = integers.stream().mapToInt(Integer::intValue).sum();
+        Predicate<Integer> isEven = el -> el % 2 == 0;
+        Predicate<Integer> notEven = el -> el % 2 != 0;
+        if (sum % 2 == 0) {
+            return getList(integers, isEven);
+        }
+        return getList(integers, notEven);
+    }
+
+    private static List<Integer> getList(List<Integer> integers, Predicate<Integer> isEven) {
+        return integers.stream()
+                .filter(isEven)
+                .collect(Collectors.toList());
+    }
+}
